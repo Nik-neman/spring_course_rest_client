0a71d0c5f57e9b8df70039ac01569c05b8f99a31
Nikolai <nik-neman@mail.ru>
2022-09-09 21:39:23 +0200

    create REST Client

--- src/main/java/com/nikolaikozlovskii/spring/rest/App.java
+++ src/main/java/com/nikolaikozlovskii/spring/rest/App.java
@@ -1,11 +1,29 @@
 package com.nikolaikozlovskii.spring.rest;
 
 
-public class App 
+import com.nikolaikozlovskii.spring.rest.configuration.MyConfig;
+import com.nikolaikozlovskii.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class App
 {
     public static void main( String[] args )
     {
-        
-        System.out.println( "Hello World!" );
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+//        List<Employee> allEmployees = communication.getAllEmployees();
+//        System.out.println(allEmployees);
+
+//        Employee empById = communication.getEmployee(1);
+//        System.out.println(empById);
+
+//        Employee emp = new Employee("Sveta", "Sokolova", "IT", 1500);
+//        emp.setId(14);
+//        communication.saveEmployee(emp);
+
+        communication.deleteEmployee(14);
     }
 }
--- src/main/java/com/nikolaikozlovskii/spring/rest/configuration/MyConfig.java
+++ src/main/java/com/nikolaikozlovskii/spring/rest/configuration/MyConfig.java
@@ -1,5 +1,17 @@
 package com.nikolaikozlovskii.spring.rest.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@ComponentScan("com.nikolaikozlovskii.spring.rest")
 public class MyConfig {
     
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 }
